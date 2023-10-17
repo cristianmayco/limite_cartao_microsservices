a732c6f5cdc5d2f095c0f4322a88e686fff29718
Cristian Mayco da Costa <cristianmayco@gmail.com>
2023-10-17 18:52:41 -0300

    Adicionado endpoint de clientes

--- msclientes/msclientes/src/main/java/com/example/msclientes/application/ClientesResource.java
+++ msclientes/msclientes/src/main/java/com/example/msclientes/application/ClientesResource.java
@@ -1,18 +1,45 @@
 package com.example.msclientes.application;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.msclientes.application.representation.ClienteSaveRequest;
+import com.example.msclientes.domain.Cliente;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("clientes")
+@RequiredArgsConstructor
 public class ClientesResource {
+
+    private final ClienteService service;
 
     @GetMapping
     public String status() {
         return "ok";
     }
 
+    @PostMapping
+    public ResponseEntity save(@RequestBody ClienteSaveRequest request) {
+        Cliente model = request.toModel();
+        service.save(model);
+        URI headerLocation = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .query("cpf={cpf}")
+                .buildAndExpand(model.getCpf())
+                .toUri();
+        return ResponseEntity.created(headerLocation).build();
+    }
 
+    @GetMapping
+    public ResponseEntity dadosCliente(@RequestParam String cpf) {
+        var cliente = service.getByCpf(cpf);
+        if (cliente.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(cliente);
+    }
 
 }
