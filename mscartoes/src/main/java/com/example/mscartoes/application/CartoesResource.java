744e3264850f69221d9041ad645bc7f07332c049
Cristian Mayco da Costa <cristianmayco@gmail.com>
2023-10-20 14:50:20 -0300

    Adicionado service, repository e método de cadastrar cartões

--- mscartoes/src/main/java/com/example/mscartoes/application/CartoesResource.java
+++ mscartoes/src/main/java/com/example/mscartoes/application/CartoesResource.java
@@ -1,16 +1,29 @@
 package com.example.mscartoes.application;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.mscartoes.application.representation.CartaoSaveRequest;
+import com.example.mscartoes.domain.Cartao;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("cartoes")
+@RequiredArgsConstructor
 public class CartoesResource {
+
+    private final CartaoService service;
 
     @GetMapping
     public String status() {
         return "ok";
+    }
+
+    @PostMapping
+    public ResponseEntity cadastrar(@RequestBody CartaoSaveRequest request){
+        Cartao cartao = request.toModel();
+        service.save(cartao);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
 }
---
+++ mscartoes/src/main/java/com/example/mscartoes/domain/BandeiraCartao.java
@@ -0,0 +1,5 @@
+package com.example.mscartoes.domain;
+
+public enum BandeiraCartao {
+    MASTER_CARD,VISA;
+}
